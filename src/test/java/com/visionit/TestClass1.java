4f913c0e486072f790e2a9fce98042d499e8acda
akashdktyagi <akashdktyagi@gmail.com>
2020-12-19 00:23:27 +0800

    first commit

---
+++ src/test/java/com/visionit/TestClass1.java
@@ -0,0 +1,17 @@
+package com.visionit;
+
+public class TestClass1 {
+
+    public void test_01(){
+        System.out.println("I am test method 1");
+    }
+
+    public void test_02(){
+        System.out.println("I am test method 1");
+    }
+
+    public void test_03(){
+        System.out.println("I am test method 1");
+    }
+
+}
